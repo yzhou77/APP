08ad45303c03c8f7d0bb22ea7bb8b46800d63d02
Yixi Zhou <yixizhou@Yixis-MacBook-Pro.local>
2019-12-03 15:14:50 -0500

    update files

--- NGO/settings.py
+++ NGO/settings.py
@@ -116,8 +116,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+CART_SESSION_ID='Event'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL='new/'
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- User/models.py
+++ User/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Myuser(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    username = models.CharField(max_length=50, default="")
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.lastName
